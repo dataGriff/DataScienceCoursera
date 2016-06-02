89ebfe9a8dc57c0a6fe87cb3a1c4a258e606ade4
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-06-02 19:20:59 +0100

    added functions and data for assignment in week 2 for pollutant

---
+++ Functions/corr.R
@@ -0,0 +1,18 @@
+corr <- function(directory, threshold = 0){
+  ## 'directory is a character vector of length 1 indicating
+  ## the locaiton of the CSV files
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) requireed to compute the correlation between
+  ## notrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  ## NOTE: Do not round the result!
+  
+  ##You will need to use the function cor in this
+  ##use ?cor to know what it does
+  
+  
+  
+}
