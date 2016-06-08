b971eb6608052ad11e9384e1d7b00847dbc1570a
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-06-08 19:23:08 +0100

    finished assignment module 3 course 2 cacheMatrix

--- Functions/cachematrix.R
+++ Functions/cachematrix.R
@@ -1,16 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function caches the inverse of a matrix
+## and the second function will use this cached matrix and return it if available in cache
+## or creates it if it is not available
 
-## Write a short comment describing this function
+## This function caches the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   
-  m <- NULL
+  inverseMatrix <- NULL
   
   ## 1.set the value of the matrix
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    inverseMatrix <<- NULL
   }
   
   ## 2.get the value of the matrix
@@ -20,38 +21,57 @@ makeCacheMatrix <- function(x = matrix()) {
   
   ## 3.set the value of the inverse
   setsolve<- function(solve) {
-    m <<- solve
+    inverseMatrix <<- solve
   }
   
   ## 4.get the value of the mean
   getsolve <- function() {
-    m
+    inverseMatrix
   }
   
   ##5. Returns list of functions
-  list(set = set, get = get,
+  list(set = set, 
+       get = get,
        setsolve = setsolve,
        getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of a matrix if cached or creates inverse if not cached
 
-cacheSolve <- function(x = matrix(), ...) {
+cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
-  m <- x$getsolve()
+  inverseMatrix <- x$getsolve()
   
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+  if(!is.null(inverseMatrix)) { #if matrix is not null (i.e. cahced) then return matrix
+    message("User Information: Getting cached data.")
+    return(inverseMatrix)
   }
+  ##else we need to get the input matrix
+  
+  message("User Information: Create the matrix as it is not in the cache.")
   
   data <- x$get()
   
-  m <- solve(data, ...)
+  ##solve it 
+  inverseMatrix <- solve(data, ...)
   
-  x$setsolve(m)
+  x$setsolve(inverseMatrix)
   
-  m
+  inverseMatrix
 }
+
+## Example input logic for console to test #############################
+## setwd("~/Projects/DataScienceCoursera")
+##; source("Functions/cachematrix.R")
+##; myMatrix <- matrix (1:4, nrow= 2, ncol = 2)
+##; x = makeCacheMatrix(myMatrix)
+##; print("Run No.1")
+##; cacheSolve(x) ##Will show mesage of creating matrix as not in cache
+##; print("Run No.2")
+##; cacheSolve(x) ##Will show mesage of getting cached data
+
+
+
+
